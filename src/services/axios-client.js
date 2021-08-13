9fafeac8b9f11237437f7b9e5885f3acd7f39ab7
huycongxe <huycongxe@gmail.com>
2021-08-13 23:30:35 +0700

    deploy

--- src/services/axios-client.js
+++ src/services/axios-client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-var baseURL = 'http://127.0.0.1:8000/'
+// var baseURL = 'http://127.0.0.1:8000/'
+var baseURL = 'http://doibong-deploy.eba-6znexcnp.ap-southeast-1.elasticbeanstalk.com/'
 
 const axiosClient = axios.create({
     baseURL: baseURL,
